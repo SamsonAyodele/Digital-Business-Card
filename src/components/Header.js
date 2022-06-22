72271675f71638f9a7ef2bc7c12f6205f0daa173
Samson Ayodele <idowusamson0777@gmail.com>
2022-06-22 14:18:42 +0100

    Digital React card

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import image from "../Samson.jpeg";
+
+export default function Header() {
+    return (
+        <img src={image} />
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <h2>Samson Ayodele</h2>
+            <p>Frontend Developer</p>
+            <a href="https://github.com/SamsonAyodele">GitHub</a>
+        </main>
+    )
+}
