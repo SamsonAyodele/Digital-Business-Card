5b9f316efefbb5428aeded219a5caf6f9bda3948
Samson Ayodele <idowusamson0777@gmail.com>
2022-06-23 20:59:48 +0100

    Digital

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+        </footer>
+    )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,17 @@ import React from "react";
 export default function Main() {
     return (
         <main>
-            <h2>Samson Ayodele</h2>
-            <p>Frontend Developer</p>
+            <h2 className="card--name">Samson Ayodele</h2>
+            <p className="card--skill">Frontend Developer</p>
             <a href="https://github.com/SamsonAyodele">GitHub</a>
+            <button type="button">Email</button>
+            <button type="button">Linkedin</button>
+            <section>
+                <h1>About</h1>
+                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+                <h1>interest</h1>
+                <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            </section>
         </main>
     )
 }
