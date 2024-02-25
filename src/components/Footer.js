23a2f2063636c146f1d4929f0a69c2a2851931d8
Samson-Ayodele <idowusamson0777@gmail.com>
2024-02-25 03:31:49 +0100

    feat: Modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,26 @@
 import React from "react";
-import facebook from "../facebook.png"
-import linkedin from "../linkedin.png"
-import twitter from "../twitter.png"
-import whatsapp from "../whatsapp.png"
+import facebook from "../facebook.png";
+import linkedin from "../linkedin.png";
+import twitter from "../twitter.png";
+import whatsapp from "../whatsapp.png";
 
 export default function Footer() {
-    return (
-        <footer>
-               <div className="socials">
-                   <a href="https://www.facebook.com/AyodeleNotime"><img src={facebook} alt="fbk"/></a>
-                   <a href="https://www.linkedin.com/in/samson-ayodele-idowu-090497193/"><img src={linkedin} alt="linkdin"/></a>
-                   <a href="https://twitter.com/AyodeleNoTime"><img src={twitter} alt="twitter"/></a>
-                   <a href="https://wa.me/08032123930"><img src={whatsapp} alt="whatsapp"/></a>
-               </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="socials">
+        <a href="https://www.facebook.com/AyodeleNotime">
+          <img src={facebook} alt="fbk" />
+        </a>
+        <a href="https://www.linkedin.com/in/samson-ayodele-idowu-090497193/">
+          <img src={linkedin} alt="linkedin" />
+        </a>
+        <a href="https://twitter.com/AyodeleNoTime">
+          <img src={twitter} alt="twitter" />
+        </a>
+        <a href="https://wa.me/08032123930">
+          <img src={whatsapp} alt="whatsapp" />
+        </a>
+      </div>
+    </footer>
+  );
 }
